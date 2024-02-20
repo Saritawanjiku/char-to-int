07cb8b27b1cb9aca3fc7087141531fc0f8e9866a
Saritawanjiku <157991202+Saritawanjiku@users.noreply.github.com>
2024-02-20 10:25:14 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char charValue = 'b';
+    int intValue = (int)charValue;
+
+    printf("Char: %c\n", charValue);
+    printf("Int: %d\n", intValue);
+
+    return 0;
+}
